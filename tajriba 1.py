8a5f7669ec6fedccb9a645d2ee09f36c8cb00c32
Sevinch0 <123802224+Sevinch0@users.noreply.github.com>
2023-01-28 13:34:31 +0500

    Add files via upload

---
+++ tajriba 1.py
@@ -0,0 +1,6 @@
+a=float(input('musbat sonni kiriting: a= '))
+b=float(input('musbat sonni kiriting: b= '))
+if(a>0 and b>0):
+    print(f'orta geometrigi {(a*b)**0.5} ')
+else: 
+    print('xatolik')
---
+++ tajriba 2.py
@@ -0,0 +1,5 @@
+h=float(input('balandlikni kiriting: h= '))
+m=float(input('massani kiriting: m= '))
+g=10
+t=(2*h/g)**0.5
+print(f'jismning tushish vaqti t= {t}')
